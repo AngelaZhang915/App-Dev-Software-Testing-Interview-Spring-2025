807f6e903bc0bd8cd8b113952f1d956a68f4477d
Angela Zhang <96925864+AngelaZhang915@users.noreply.github.com>
2025-02-05 20:41:35 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("loginForm").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password })
+    });
+
+    const messageDiv = document.getElementById("message");
+  
+    messageDiv.innerHTML = await response.text();
+});
